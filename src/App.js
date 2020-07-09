6b41455d3cce931eff4b94cfe0ee6c8ae5d48374
ches4117 <ches4117@gmail.com>
2020-07-09 17:12:35 +0800

    [feat] Add app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+import Navigation from './navigation'
+import Article from './article'
+import Footer from './footer'
+
+
+const Body = styled.div`
+  background: #F3F4F7;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  flex-flow: row;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navigation />
+      <Article />
+      <Footer />
+    </Fragment>
   );
 }
 
