e272a55647b3a8e92a042082d61bdb3e8ab85a43
ches4117 <ches4117@gmail.com>
2020-07-09 17:11:42 +0800

    [feat] Add footer

---
+++ src/footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterCompon = styled.footer`
+  width: 100vw;
+  padding: 10px;
+  background: #fff;
+  font-size: 14px;
+  display: flex;
+  justify-content: center;
+  flex: 1;
+`;
+
+function Footer() {
+  return (
+    <FooterCompon>
+      ches4117
+    </FooterCompon>
+  );
+}
+
+export default Footer;
