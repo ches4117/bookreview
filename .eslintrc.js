b060cf05d0da8efd85888a1ef5cf231951cb90f9
ches4117 <ches4117@gmail.com>
2022-03-12 09:05:28 +0800

    feat: asjust eslint and prettierr setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,16 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2020': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 11,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
-    },
-    'overrides': [
-        {
-            files: [
-                '**/*.test.js'
-            ],
-            env: {
-                jest: true // now **/*.test.js files' env has both es6 *and* jest
-            },
-            // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
-            // "extends": ["plugin:jest/recommended"]
-            plugins: ['jest'],
-            rules: {
-                'jest/no-disabled-tests': 'warn',
-                'jest/no-focused-tests': 'error',
-                'jest/no-identical-title': 'error',
-                'jest/prefer-to-have-length': 'warn',
-                'jest/valid-expect': 'error'
-            }
-        }
-    ],
-}
+  env: {
+    es6: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  plugins: ['prettier'],
+};
